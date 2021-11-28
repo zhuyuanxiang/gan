37ee433fef390d0ce472f53cd94c6ec0b0a3e6d2
zyx <zhuyuanxiang@gmail.com>
2021-11-28 11:50:14 +0800

    Update gan_mnist.py
    完成了 GANDiscriminator() 类
    解决了重新设置模型时遇到的问题（需要重新设置优化器，导入新的参数）

--- gan_mnist.py
+++ gan_mnist.py
@@ -1,10 +1,3 @@
-# -*- encoding: utf-8 -*-
-
-from datasets import MnistDataset
-from linear_classifier import AdamLayerNormLinearClassifier
-from linear_classifier import test_network
-from linear_classifier import train_network
-
 """
 =================================================
 @path   : gan -> gan_mnist.py
@@ -18,13 +11,69 @@ from linear_classifier import train_network
 ==================================================
 """
 import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+import pandas, numpy, random
+from gan_simple import Discriminator
+from gan_simple import Generator
 import matplotlib.pyplot as plt
 from datetime import datetime
+from datasets import generate_random, mnist_dataset
+
+
+class GANDiscriminator(Discriminator):
+    def __init__(self):
+        super(GANDiscriminator, self).__init__()
+        # 重新设置模型后，必须重新设置优化器，否则就使用了父模型的参数（这点我其实挺迷惑的，又不报错，但是结果不对）
+        self.model = nn.Sequential(
+                nn.Linear(784, 200),
+                nn.Sigmoid(),
+                nn.Linear(200, 1),
+                nn.Sigmoid()
+        )
+        self.loss_function = nn.MSELoss()
+        self.optimiser = torch.optim.SGD(self.parameters(), lr=0.01)
+
+
+def test_discriminator():
+    D = GANDiscriminator()
+    for label, image_data_tensor, target_tensor in mnist_dataset:
+        D.train(image_data_tensor, torch.FloatTensor([1.0]))
+        D.train(generate_random(784), torch.FloatTensor([0.0]))
+        pass
+    D.plot_progress()
+
+    print("--->对真实数据的判断结果<---")
+    for i in range(4):
+        image_data_tensor = mnist_dataset[random.randint(0, 60000)][1]
+        print(D.forward(image_data_tensor).item())
+        pass
+
+    print("--->对伪装数据的判断结果<---")
+    for i in range(4):
+        image_fake_tensor = generate_random(784)
+        print(D.forward(image_fake_tensor).item())
+        pass
+    pass
+
+
+class GANGenerator(Generator):
+    def __init__(self):
+        super(GANGenerator, self).__init__()
+
+        self.model = nn.Sequential(
+                nn.Linear(1, 200),
+                nn.Sigmoid(),
+                nn.Linear(200, 784),
+                nn.Sigmoid()
+        )
+
+        pass
 
 
 def main(name):
     print(f'Hi, {name}', datetime.now())
-
+    test_discriminator()
     pass
 
 
